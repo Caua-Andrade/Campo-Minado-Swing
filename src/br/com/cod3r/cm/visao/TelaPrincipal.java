16a5504da2742be45a32deec797d36fe5957c608
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-16 08:42:26 -0300

    Criei o construtor e na class extendi o JFrame

---
+++ src/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -0,0 +1,17 @@
+package br.com.cod3r.cm.visao;
+
+import javax.swing.*;
+
+public class TelaPrincipal extends JFrame {
+
+    public TelaPrincipal() {
+        // métodos do JFrame
+        setTitle("Campo Minado");
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        new TelaPrincipal();
+    }
+}

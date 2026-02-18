cfa70c9f0afb3161e0a0e17b7ed472e5a1eb970f
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-18 07:29:48 -0300

    criei o switch

--- src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -1,12 +1,46 @@
 package br.com.cod3r.cm.visao;
 
 import br.com.cod3r.cm.modelo.Campo;
+import br.com.cod3r.cm.modelo.CampoEvento;
+import br.com.cod3r.cm.modelo.CampoObservador;
 
 import javax.swing.*;
 
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
 
     public BotaoCampo(Campo campo) {
 
+    }
+
+    @Override
+    public void eventoOcorreu(Campo campo, CampoEvento evento) {
+        switch (evento) {
+            case ABRIR: {
+                aplicarEstiloAbrir();
+                break;
+            }
+            case MARCAR: {
+                aplicarEstiloMarcar();
+                break;
+            }
+            case EXPLODIR: {
+                aplicarEstiloExplodir();
+                break;
+            }
+            default:
+                aplicarEstiloPadrao();
+        }
+    }
+
+    private void aplicarEstiloPadrao() {
+    }
+
+    private void aplicarEstiloExplodir() {
+    }
+
+    private void aplicarEstiloMarcar() {
+    }
+
+    private void aplicarEstiloAbrir() {
     }
 }
